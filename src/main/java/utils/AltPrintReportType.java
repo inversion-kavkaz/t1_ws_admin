8a4298d9a8c1f5914fabab733686f90bfc769cd3
bvv <bvv@inversion-kavkaz.ru>
2020-02-10 15:08:48 +0300

    initial commit admin ver. 1.0.0.0

--- src/main/java/utils/AltPrintReportType.java
+++ src/main/java/utils/AltPrintReportType.java
@@ -1,8 +1,6 @@
 package utils;
 
 public class AltPrintReportType {
-    public static final int PRINT_WS_TYPE = 200090;
-    public static final int PRINT_WS_LOG_TYPE = 200091;
-//    public static final int PRINT_ACC_TYPE = 200071;
-//    public static final int PRINT_OBD_TYPE = 200072;
+    public static final int PRINT_WS_TYPE = 200050;
+    public static final int PRINT_WS_LOG_TYPE = 200051;
 }
