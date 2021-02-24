e5e4fa90d63bba3ab8752493796af2a63208ee0b
bvv <bvv@inversion-kavkaz.ru>
2021-02-24 22:53:28 +0300

    добавлена поддержка журнала online сверки с функциями отправки запросов через внешние jar модули сверки платежей провайдеров

--- src/main/java/ru/inversionkavkaz/dnrwsadm/vrfreq/controller/EditVIkVrfReqController.java
+++ src/main/java/ru/inversionkavkaz/dnrwsadm/vrfreq/controller/EditVIkVrfReqController.java
@@ -17,29 +17,31 @@ import ru.inversionkavkaz.dnrwsadm.entity.PVerifyRequest;
 import ru.inversionkavkaz.dnrwsadm.utils.DbUtils;
 import ru.inversionkavkaz.dnrwsadm.utils.LovUtils;
 import ru.inversionkavkaz.dnrwsadm.vrfreq.entity.PVIkVrfReq;
-import ru.inversionkavkaz.payverifierscore.ProgramOption;
-import ru.inversionkavkaz.payverifierscore.Starter;
 
 import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 /**
- * @author  porche
- * @since   Mon Feb 15 16:50:47 MSK 2021
+ * @author porche
+ * @since Mon Feb 15 16:50:47 MSK 2021
  */
-public class EditVIkVrfReqController extends JInvFXFormController <PVIkVrfReq>
-{  
-//    @FXML JInvLongField ID;
+public class EditVIkVrfReqController extends JInvFXFormController<PVIkVrfReq> {
+    //    @FXML JInvLongField ID;
 //    @FXML JInvTextField DCREATE;
-    @FXML JInvTextField SERVICENAME;
-    @FXML JInvTextField SERVICEDESCRIPTION;
-//    @FXML JInvTextField DATESTART;
+    @FXML
+    JInvTextField SERVICENAME;
+    @FXML
+    JInvTextField SERVICEDESCRIPTION;
+    //    @FXML JInvTextField DATESTART;
 //    @FXML JInvTextField DATEEND;
-    @FXML JInvComboBox PAYELEMENTID;
+    @FXML
+    JInvComboBox PAYELEMENTID;
 
 
 //    @FXML JInvTextField STATUS;
@@ -57,13 +59,12 @@ public class EditVIkVrfReqController extends JInvFXFormController <PVIkVrfReq>
 //
 
     @Override
-    protected void init () throws Exception 
-    {
-        super.init ();
+    protected void init() throws Exception {
+        super.init();
         initLov();
     }
 
-    private void initLov(){
+    private void initLov() {
         JInvEntityLov<PIkOvDnrService, String> lov = LovUtils.bindLov(PIkOvDnrService.class, String.class, SERVICENAME, SERVICEDESCRIPTION,
                 ResourceBundle.getBundle("ru/inversionkavkaz/dnrwsadm/vrfreq/controller/res/PIkOvDnrService"));
         lov.checkValue(getDataObject().getSERVICENAME(), true);
@@ -72,7 +73,7 @@ public class EditVIkVrfReqController extends JInvFXFormController <PVIkVrfReq>
             @Override
             public Object call(PIkOvDnrService ikOvDnrService) {
                 PAYELEMENTID.getItems().clear();
-                if(ikOvDnrService.getPAYELEMENTIDS()!=null){
+                if (ikOvDnrService.getPAYELEMENTIDS() != null) {
                     PAYELEMENTID.getItems().addAll(ikOvDnrService.getPAYELEMENTIDS().split(","));
                 }
                 PAYELEMENTID.setDisable(PAYELEMENTID.getItems().isEmpty());
@@ -89,10 +90,10 @@ public class EditVIkVrfReqController extends JInvFXFormController <PVIkVrfReq>
 //        lov.bindControl(getDataObject().getURL(), PIkOvDnrService::getCOMPARISONURL);
     }
 
-    String buildRunArgs(PVIkVrfReq pvIkVrfReq){
+    String buildRunArgs(PVIkVrfReq pvIkVrfReq) {
 //        String connectionString = getTaskContext().getConnectionString(ConnectionStringFormatEnum.SQL_SIMPLE);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        String connectionString =getTaskContext().getConnectionString(ConnectionStringFormatEnum.SQL_PLUS);
+        String connectionString = getTaskContext().getConnectionString(ConnectionStringFormatEnum.SQL_PLUS);
         String serviceName = "VRF";
         String url = "http:\\test.com";
         String args = String.format("-o %s -c %s -s %s -f %s -u %s -t %s", connectionString, serviceName,
@@ -106,55 +107,42 @@ public class EditVIkVrfReqController extends JInvFXFormController <PVIkVrfReq>
         payVerifierTask = new BiCompTask() {
             @Override
             protected Object call() throws Exception {
-                System.out.println("call 1");
                 File file = new File("i://japp//citypayverifier-1.0-SNAPSHOT-jar-with-dependencies.jar");
-                System.out.println("call 2");
-                URLClassLoader child = new URLClassLoader (new URL[] {file.toURI().toURL()}, ClassLoader.getSystemClassLoader());
-                System.out.println("call 3");
-                Class<ru.inversionkavkaz.payverifierscore.Starter> clazz = (Class<Starter>) Class.forName("ru.inversionkavkaz.citypayverifier.Main", true, child);
-                ru.inversionkavkaz.payverifierscore.Starter starter = clazz.newInstance();
+                URLClassLoader child = new URLClassLoader(new URL[]{file.toURI().toURL()}, ClassLoader.getSystemClassLoader());
+                Class classToLoad = Class.forName("ru.inversionkavkaz.citypayverifier.Main", true, child);
+                Class programOptionsClass = Class.forName("ru.inversionkavkaz.payverifierscore.ProgramOption", true, child);
 
-                System.out.println("call 4");
-                java.lang.reflect.Method method = starter.getClass().getMethod("send", ru.inversionkavkaz.payverifierscore.ProgramOption.class);
-                System.out.println("call 5");
-
-                ProgramOption programOption = new ProgramOption();
-                programOption.connection = connectionString;// "xxi/NEW8I@odb12";
-                programOption.cpsevdo = getDataObject().getSERVICENAME();//  "VRF";
-                programOption.dateStart = getDataObject().getDATESTART().format(formatter);// "20190101235959";
-                programOption.dateEnd = getDataObject().getDATEEND().format(formatter);//"20201118235959";
+                Method method = classToLoad.getDeclaredMethod("send", Map.class);
+                Object instance  = classToLoad.newInstance();
+                Map<String, String> programOptions = new HashMap();
+                programOptions.put("o",connectionString);
+                programOptions.put("c", getDataObject().getSERVICENAME());
+                programOptions.put("s",getDataObject().getDATESTART().format(formatter));
+                programOptions.put("f",getDataObject().getDATEEND().format(formatter));
                 if(getDataObject().getPAYELEMENTID()!=null)
-                    programOption.payElementID = getDataObject().getPAYELEMENTID().toString();
-                programOption.transactionID =  getDataObject().getID().toString();//"7";
-                programOption.reportOutDir = "X"; //не выгружать отчет, только сформировать
+                    programOptions.put("e",getDataObject().getPAYELEMENTID().toString());
+                programOptions.put("t",getDataObject().getID().toString());
+                programOptions.put("d","X");
 
-                System.out.println("call 6");
-                String result = (String) method.invoke(starter, programOption);
-                System.out.println("call 7");
-
+                String result = (String) method.invoke(instance, programOptions);
                 System.out.println("result="+result);
                 if(result!=null){
                     throw new RuntimeException("Ошибка внешнего процесса: " + result);
                 }
-
-
-//                Object instance = classToLoad.newInstance();
+//                Object instance2 = programOptionsClass.newInstance();
 //                Object result = method.invoke(instance);
-
-//                Class classToLoad = Class.forName("ru.inversionkavkaz.citypayverifier.Main", true, child);
-//                Method method = classToLoad.getDeclaredMethod("myMethod");
-//                Object instance = classToLoad.newInstance();
-//                Object result = method.invoke(instance);
-
-
-//                System.out.println("in call 1");
-//                Class<ru.inversionkavkaz.payverifierscore.Starter> clazz = (Class<Starter>) Class.forName("ru.inversionkavkaz.citypayverifier.Main");
-//                System.out.println("in call 2");
+//                System.out.println("call 1");
+//                File file = new File("i://japp//citypayverifier-1.0-SNAPSHOT-jar-with-dependencies.jar");
+//                System.out.println("call 2");
+//                URLClassLoader child = new URLClassLoader (new URL[] {file.toURI().toURL()}, ClassLoader.getSystemClassLoader());
+//                System.out.println("call 3");
+//                Class<ru.inversionkavkaz.payverifierscore.Starter> clazz = (Class<Starter>) Class.forName("ru.inversionkavkaz.citypayverifier.Main", true, child);
 //                ru.inversionkavkaz.payverifierscore.Starter starter = clazz.newInstance();
-//                System.out.println("in call 3");
-//                java.lang.reflect.Method method;
-//                method = starter.getClass().getMethod("send", ru.inversionkavkaz.payverifierscore.ProgramOption.class);
-//                System.out.println("in call 4");
+//
+//                System.out.println("call 4");
+//                java.lang.reflect.Method method = starter.getClass().getMethod("send", ru.inversionkavkaz.payverifierscore.ProgramOption.class);
+//                System.out.println("call 5");
+//
 //                ProgramOption programOption = new ProgramOption();
 //                programOption.connection = connectionString;// "xxi/NEW8I@odb12";
 //                programOption.cpsevdo = getDataObject().getSERVICENAME();//  "VRF";
@@ -164,54 +152,91 @@ public class EditVIkVrfReqController extends JInvFXFormController <PVIkVrfReq>
 //                    programOption.payElementID = getDataObject().getPAYELEMENTID().toString();
 //                programOption.transactionID =  getDataObject().getID().toString();//"7";
 //                programOption.reportOutDir = "X"; //не выгружать отчет, только сформировать
-//                System.out.println("in call 5");
+//
+//                System.out.println("call 6");
 //                String result = (String) method.invoke(starter, programOption);
-//                System.out.println("in call 6");
+//                System.out.println("call 7");
+//
 //                System.out.println("result="+result);
-////                String result =  new ru.inversionkavkaz.citypayverifier.Main().send(programOption);
 //                if(result!=null){
 //                    throw new RuntimeException("Ошибка внешнего процесса: " + result);
 //                }
+//
+//
+////                Object instance = classToLoad.newInstance();
+////                Object result = method.invoke(instance);
+//
+////                Class classToLoad = Class.forName("ru.inversionkavkaz.citypayverifier.Main", true, child);
+////                Method method = classToLoad.getDeclaredMethod("myMethod");
+////                Object instance = classToLoad.newInstance();
+////                Object result = method.invoke(instance);
+//
+//
+////                System.out.println("in call 1");
+////                Class<ru.inversionkavkaz.payverifierscore.Starter> clazz = (Class<Starter>) Class.forName("ru.inversionkavkaz.citypayverifier.Main");
+////                System.out.println("in call 2");
+////                ru.inversionkavkaz.payverifierscore.Starter starter = clazz.newInstance();
+////                System.out.println("in call 3");
+////                java.lang.reflect.Method method;
+////                method = starter.getClass().getMethod("send", ru.inversionkavkaz.payverifierscore.ProgramOption.class);
+////                System.out.println("in call 4");
+////                ProgramOption programOption = new ProgramOption();
+////                programOption.connection = connectionString;// "xxi/NEW8I@odb12";
+////                programOption.cpsevdo = getDataObject().getSERVICENAME();//  "VRF";
+////                programOption.dateStart = getDataObject().getDATESTART().format(formatter);// "20190101235959";
+////                programOption.dateEnd = getDataObject().getDATEEND().format(formatter);//"20201118235959";
+////                if(getDataObject().getPAYELEMENTID()!=null)
+////                    programOption.payElementID = getDataObject().getPAYELEMENTID().toString();
+////                programOption.transactionID =  getDataObject().getID().toString();//"7";
+////                programOption.reportOutDir = "X"; //не выгружать отчет, только сформировать
+////                System.out.println("in call 5");
+////                String result = (String) method.invoke(starter, programOption);
+////                System.out.println("in call 6");
+////                System.out.println("result="+result);
+//////                String result =  new ru.inversionkavkaz.citypayverifier.Main().send(programOption);
+////                if(result!=null){
+////                    throw new RuntimeException("Ошибка внешнего процесса: " + result);
+////                }
                 return this;
             }
         };
-        payVerifierTask.setOnFailed (this::onFailed);
-        payVerifierTask.setOnCancelled (this::onCancelled);
-        payVerifierTask.setOnSucceeded (this::onSucceded);
+        payVerifierTask.setOnFailed(this::onFailed);
+        payVerifierTask.setOnCancelled(this::onCancelled);
+        payVerifierTask.setOnSucceeded(this::onSucceded);
 
-        new Thread (payVerifierTask, "Verification in progress").start ();
-        setState (StateEnum.WAIT);
+        new Thread(payVerifierTask, "Verification in progress").start();
+        setState(StateEnum.WAIT);
     }
 
     private void onCancelled(Event event) {
-        setState (StateEnum.ACTIVE);
+        setState(StateEnum.ACTIVE);
         String errorMessage = "Внешний процесс сверки приостановлен. ";
-        if(payVerifierTask!=null && payVerifierTask.getException()!=null&&payVerifierTask.getException().getMessage()!=null){
-            errorMessage+=this.getException().getMessage();
+        if (payVerifierTask != null && payVerifierTask.getException() != null && payVerifierTask.getException().getMessage() != null) {
+            errorMessage += this.getException().getMessage();
             payVerifierTask.getException().printStackTrace();
         }
         System.out.println(errorMessage);
-        Alerts.error(this, "Выполнение внешнего процесса сверкиЖ","Ошибка при выполнении внешнего процесса сверки",errorMessage);
+        Alerts.error(this, "Выполнение внешнего процесса сверкиЖ", "Ошибка при выполнении внешнего процесса сверки", errorMessage);
     }
 
     private void onFailed(Event event) {
-        setState (StateEnum.ACTIVE);
+        setState(StateEnum.ACTIVE);
         String errorMessage = "Внешний процесс сверки завершен с ошибкой. ";
-        if(payVerifierTask!=null && payVerifierTask.getException()!=null){
-            errorMessage+=payVerifierTask.getException().getMessage();
+        if (payVerifierTask != null && payVerifierTask.getException() != null) {
+            errorMessage += payVerifierTask.getException().getMessage();
             payVerifierTask.getException().printStackTrace();
         }
         System.out.println(errorMessage);
-        Alerts.error(this, "Выполнение внешнего процесса сверки","Ошибка при выполнении внешнего процесса сверки",errorMessage);
+        Alerts.error(this, "Выполнение внешнего процесса сверки", "Ошибка при выполнении внешнего процесса сверки", errorMessage);
 
-        if(payVerifierTask!=null && payVerifierTask.getException()!=null && payVerifierTask.getException().getClass()==RuntimeException.class)
-            closeNow (FormReturnEnum.RET_OK);
+        if (payVerifierTask != null && payVerifierTask.getException() != null && payVerifierTask.getException().getClass() == RuntimeException.class)
+            closeNow(FormReturnEnum.RET_OK);
     }
 
     private void onSucceded(Event event) {
         System.out.println("Получение реестра платежей от провайдера завершено.");
         Alerts.info(this, "Получение реестра платежей от провайдера завершено.");
-        closeNow (FormReturnEnum.RET_OK);
+        closeNow(FormReturnEnum.RET_OK);
     }
 
     @Override
